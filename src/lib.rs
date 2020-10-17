76be68f28597f541101fd4cad51bcae52b08693b
Catry <catry.me@gmail.com>
2020-10-17 13:09:59 +0900

    fix ratelimit counter

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ const POST_BOT_SERVERS: &str = "/bots/servers";
 /// println("{}", koreanbots_rs::get_bots("1"));
 /// ```
 pub fn get_bots(page: Option<&str>) -> ureq::SerdeValue {
-    check();
+    check(BASE);
     let resp = ureq::get(&(BASE.to_owned() + GET_BOTS + "?page=" + page.unwrap_or("1")))
         .set("content-type","application/json").call().into_json().unwrap();
     if resp["code"] == 200 {
@@ -32,7 +32,7 @@ pub fn get_bots(page: Option<&str>) -> ureq::SerdeValue {
 /// println("{}", koreanbots_rs::get_bots_by_category("관리", Some("1")));
 /// ```
 pub fn get_bots_by_category(category: &str, page: Option<&str>) -> ureq::SerdeValue {
-    check();
+    check(BASE);
     let resp = ureq::get(&(BASE.to_owned() + "/bots/category/" + category ))
         .set("content-type","application/json")
         .set("page", page.unwrap_or_default()).call().into_json().unwrap();
@@ -51,7 +51,7 @@ pub fn get_bots_by_category(category: &str, page: Option<&str>) -> ureq::SerdeVa
 /// println("{}", koreanbots::get_server_widget("387548561816027138"));
 /// ```
 pub fn get_server_widget(id: &str) -> std::string::String {
-    check();
+    check(BASE);
     let url = BASE.to_owned() + "/widget/bots/servers/" + id +".svg";
     return url.to_owned();
 }
@@ -64,7 +64,7 @@ pub fn get_server_widget(id: &str) -> std::string::String {
 /// println("{}", koreanbot_rs::getvotewidget("387548561816027138"));
 /// ```
 pub fn get_vote_widget(id: &str) -> std::string::String {
-    check();
+    check(BASE);
     let url = BASE.to_owned() + "/widget/bots/votes/" + id +".svg";
     return url.to_owned();
 }
@@ -77,7 +77,7 @@ pub fn get_vote_widget(id: &str) -> std::string::String {
 /// println("{}", koreanbots_rs::get_search_bots("원더봇", Some("1")));
 /// ```
 pub fn get_search_bots(search: &str, page: Option<&str>) -> ureq::SerdeValue {
-    check();
+    check(BASE);
     let resp = ureq::get(&(BASE.to_owned() + "/bots/search?q=" + search + "&page" + page.unwrap_or_default()))
         .set("content-type","application/json").call().into_json().unwrap();
     if resp["code"] == 200 {
@@ -95,7 +95,7 @@ pub fn get_search_bots(search: &str, page: Option<&str>) -> ureq::SerdeValue {
 /// println("{}", koreanbots_rs::get_seacrh_bots("387548561816027138"));
 /// ```
 pub fn get_bot(id: &str) -> ureq::SerdeValue {
-    check();
+    check(BASE);
     let resp = ureq::get(&(BASE.to_owned() + GET_BOTS + "/" + id ))
         .set("content-type","application/json").call().into_json().unwrap();
     if resp["code"] == 200 {
@@ -112,7 +112,7 @@ pub fn get_bot(id: &str) -> ureq::SerdeValue {
 /// println("{}", koreanbots_rs::get_votes_mybot("MY_BOT_ID", "KOREANBOTS_TOKEN"));
 /// ```
 pub fn get_votes_mybot(id: &str, token: &str) -> ureq::SerdeValue {
-    check();
+    check(BASE);
     let resp = ureq::get(&(BASE.to_owned() + GET_VOTES_MYBOT + id ))
         .set("content-type","application/json")
         .set("token", token).call().into_json().unwrap();
@@ -131,7 +131,7 @@ pub fn get_votes_mybot(id: &str, token: &str) -> ureq::SerdeValue {
 /// println("{}", koreanbots_rs::post_bot_servers("KOREANBOTS_TOKEN", 123));
 /// ```
 pub fn post_bot_servers(token: &str, guilds : usize) -> ureq::SerdeValue {
-    check();
+    check(&(BASE.to_owned() + POST_BOT_SERVERS));
     let resp = ureq::post(&(BASE.to_owned() + POST_BOT_SERVERS ))
         .set("token", token)
         .send_json(json!({ "servers": guilds })).into_json().unwrap();
@@ -142,13 +142,13 @@ pub fn post_bot_servers(token: &str, guilds : usize) -> ureq::SerdeValue {
     }
 }
 
-fn check() {
-    let resp = ureq::get(BASE)
+fn check(path: &str ) {
+    let resp = ureq::get(path)
     .set("content-type","application/json").call();
 
     if resp.header("x-ratelimit-remaining") == Some("0") || resp.header("x-ratelimit-remaining") >= resp.header("x-ratelimit-limit") {
         let secs_string = resp.header("x-ratelimit-reset").unwrap().to_string();
         let secs_number:u64 = secs_string.trim().parse().unwrap();
-       return println!("you're now rate limited. retrying after {:?}ms", Duration::from_secs(secs_number));
+       return eprintln!("you're now rate limited. retrying after {:?}ms", Duration::from_secs(secs_number));
     }
 }
